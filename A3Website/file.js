67a85aff42ee96d96b710c65eeefb9238120b22f
Thanh Hoang <thanhthanhh@student.unimelb.edu.au>
2026-05-15 23:01:06 +1000

    connected files to js

--- A3Website/file.js
+++ A3Website/file.js
@@ -4,3 +4,13 @@ const itemQuantity = [1]
 const itemInStock = [true]
 
 
+console.log("Working")
+
+var removeCartItem = document.getElementsByClassName('addremove')
+console.log(removeCartItem)
+for (var i = 0; i < removeCartItem; i++){
+    var button = removeCartItem[i]
+    button.addEventListener('click', function () {
+                console.log("clicked")
+    })
+}
