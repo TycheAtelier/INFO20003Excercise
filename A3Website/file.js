4f6a43b498289b7bee4ddb5c7c6d1a12d70e5623
Thanh Hoang <thanhthanhh@student.unimelb.edu.au>
2026-05-19 16:02:53 +1000

    organising css changes and adding more responsive elements

--- A3Website/file.js
+++ A3Website/file.js
@@ -1,16 +1,24 @@
-const itemNames = ['Adhesive']
-const itemPrices = [10]
-const itemQuantity = [1]
-const itemInStock = [true]
+const productName = "Adhesive"
 
-
-console.log("Working")
-
-var removeCartItem = document.getElementsByClassName('addremove')
-console.log(removeCartItem)
-for (var i = 0; i < removeCartItem; i++){
-    var button = removeCartItem[i]
-    button.addEventListener('click', function () {
-                console.log("clicked")
-    })
-}
+const cart = {};
+    function addToCart (productName, productPrice){
+            if (cart[productName]) {
+                cart[productName].quantity += 1;
+                cart[productName].totalPrice += productPrice;
+            } else {
+                cart[productName] = {
+                    quantity: 1,
+                    totalPrice: productPrice
+                };
+            }
+            updateCartDisplay();
+        }
+        function updateCartDisplay() {
+            const cartList = document.getElementById('cart');
+            cartList.innerHTML = '';
+            for (let product in cart) {
+                const listItem = document.createElement('li');
+                listItem.innerText = `${product} - Quantity: ${cart[product].quantity} - Total Price: Rs${cart[product].totalPrice.toFixed(2)}`;
+                cartList.appendChild(listItem);
+            }
+        }
