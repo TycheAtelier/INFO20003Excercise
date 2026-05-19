581f7b8bc70e7f3dc6bae3f415b51916493b604d
Thanh Hoang <thanhthanhh@student.unimelb.edu.au>
2026-05-19 23:30:16 +1000

    responsive design fixes

--- A3Website/file.js
+++ A3Website/file.js
@@ -1,6 +1,9 @@
-const productName = "Adhesive"
+const itemName = ["Adhesive"];
+const itemPrice = ["10"];
+const itemQuantity = [1];
+const itemInStock = [true];
 
-const cart = {};
+/* const cart = {};
     function addToCart (productName, productPrice){
             if (cart[productName]) {
                 cart[productName].quantity += 1;
@@ -22,3 +25,41 @@ const cart = {};
                 cartList.appendChild(listItem);
             }
         }
+*/
+        
+const ShoppingCart = () => {
+    const cart = [];
+
+    const addToCart = (productIndex) => {
+        if (itemInStock [itemIndex]) {
+            cart.push (itemIndex)
+            console.log('${itemName[itemIndex]} added to the cart');
+        } else {
+            console.log ('$ {itemName[itemIndex]} is out of stock');
+        }
+        }
+    }
+
+    const removeItemFromCart = (itemIndex) => {
+        const index = cart.indexOf(itemIndex);
+        if (index > -1) {
+            cart.splice(index,1);
+        }
+    };
+
+    const getCartItems = () => {
+        return cart.map (itemIndex => itemNames [itemIndex]);
+    };
+
+    const calculateTotal = () => {
+        return cart.reduce((total, itemIndex) => {
+            return total + itemPrices(itemIndex) * itemQuantity[itemIndex];
+        }, 0);
+    }
+
+    return {
+        addItemToCart,
+        removeItemFromCart,
+        getCartItems,
+        calculateTotal
+    };
